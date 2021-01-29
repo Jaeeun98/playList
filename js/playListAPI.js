7009fcb852fc24f7ad1a5ffaf5f4cf814ba78345
Jaeeun98 <wodms4525@naver.com>
2021-01-29 18:34:29 +0900

    Title : Change
    API playlist playlistAPI → videoAPI&Promise change

--- js/playListAPI.js
+++ js/playListAPI.js
@@ -1,40 +1,10 @@
 const videoBox = document.querySelector('.video');
 const playListUl = document.querySelector('.playListUl');
 const key = "AIzaSyBJFWmVhHaFhVmTyg7PQPD9EJolH1AX4Vk";
-const itemIdMv = "PL4fGSI1pDJn7rC9G6HKWK7Na4R9iu-c8O"  //mv
-const itemId = "PL4fGSI1pDJn6jXS_Tv_N9B8Z0HTRVJE0m";  //album
-const popItemId = "PL4fGSI1pDJn6O1LS0XSdF3RyO0Rq_LDeI"; //pop
-const playListApi = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${itemId}&key=${key}`;
-const ramdomIcon = document.querySelector('.fas');
-//let shuffle = false;
+const kpopListId = "PLOHoVaTp8R7dfrJW5pumS0iD_dhlXKv17";
+const popListId = "PLOHoVaTp8R7d3L_pjuwIa6nRh4tH5nI4x";
+//const ramdomIcon = document.querySelector('.fas');
 
-
-//playList
-fetch(playListApi)
-  .then(res => res.json())
-  .then(data => showPlayListItem(data));
-function showPlayListItem(data){
-  for(i=0; i<data.items.length; i++){
-    const num = i+1;
-    const title = data.items[i].snippet.title;
-    const list = document.createElement('li');
-    const videoId = data.items[i].snippet.resourceId.videoId;
-    const output = `
-        <div class="num">${num}</div>
-        <div class="title">${title}</div>
-        <div class="playBtn">
-          <div class="play">&#9654</div>
-          <div class="stop">&#9632</div>
-        </div>
-    `
-    list.innerHTML = output;
-    playListUl.appendChild(list);
-
-    //videoId className add
-    list.classList.add(videoId);
-    showVideo();
-  } 
-}
 //video iframe
 let player;
 let popPlayer;
@@ -43,7 +13,7 @@ function showVideo(){
     width:'90%',
     playerVars:{
       'modestbranding' : 1,
-      'list' : itemIdMv,
+      'list' : kpopListId,
       'listType' : "playlist",
       'autoplay' : 0,
       'controls' : 1,
@@ -51,14 +21,14 @@ function showVideo(){
       'loop' : 1
     },
     events: {
-      'onReady' : ready,
+      'onReady' : kpopReady,
     }
   })
   popPlayer = new YT.Player('popPlayer', {
     width:'90%',
     playerVars:{
       'modestbranding' : 1,
-      'list' : popPlayer,
+      'list' : popListId,
       'listType' : "playlist",
       'autoplay' : 0,
       'controls' : 1,
@@ -71,66 +41,61 @@ function showVideo(){
   })
   
 }
-//shuffle
-//ramdomIcon.addEventListener('click', function(){
-//  shuffle ? shuffle = false : shuffle = true;
-//})
+
+
+
+//load 중에 함수 호출하면 오류뜸, 준비중일때 세팅
+//kpopReady
+function kpopReady(e){
+  //playlist
+  const videoIdArr = e.target.getPlaylist();
+  const promiseList = videoIdArr.map(getItem);
+
+  function getItem(id){
+    const videoApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${key}` 
+  
+    return new Promise(res => {
+      fetch(videoApi)
+        .then(res => res.json())
+        .then(({ items }) => {
+          res(items[0].snippet);
+        })
+      })
+  }
+
+  Promise.all(promiseList)
+    .then(valuse => {
+      valuse.forEach((snippet, index) => {
+        const num = index+1;
+        const title = snippet.title;
+        const list = document.createElement('li');
+        const videoId = videoIdArr[index];
+
+        const output = `
+          <div class="num">${num}</div>
+          <div class="title">${title}</div>
+          <div class="playBtn">
+            <div class="play">&#9654</div>
+            <div class="stop">&#9632</div>
+          </div>
+          `
+        list.innerHTML = output;
+        list.classList.add(videoId);
+        playListUl.appendChild(list);
+      })
+    })
+
+
+
+
+
+
+
+
 
 //popReady
 function popReady(e){
   console.log(e.target)
 }
 
-//load 중에 함수 호출하면 오류뜸, 준비중일때 세팅
-//kpopReady
-const fetchArr = [];
-function ready(e){
-  console.log(e.target);
-
-  //playlist
-  const playlistArr = e.target.getPlaylist();
-  for(let i=0;i<5;i++){
-    const playlistVideoApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${playlistArr[i]}&key=${key}` 
-    fetch(playlistVideoApi)
-    fetchArr.push(fetch(playlistVideoApi)) //api 가져오기
-  }
-  //shuffle
-  //shuffle ? e.target.setShuffle(true) : e.target.setShuffle(false);
-}
-
-function showPlayListItme2(data){
-  console.log(data[0])
- // let titleArr = data.items[0].snippet.title;
- // console.log(titleArr);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-//search
-const searchApi = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=date&type=playlist&key=${key}`
-  fetch(searchApi)
-    .then(res => res.json)
-    .then(data => titleSort(data))
-function titleSort(data){
-  console.log(data);
-}
-
-*/
-
-//playListClick
 
