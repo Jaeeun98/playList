1be5d64f7d77dda82f141ba1ac5795ce7332da53
Jaeeun98 <wodms4525@naver.com>
2021-01-30 19:03:06 +0900

    Title : Add
    API pop playlist add

--- js/playListAPI.js
+++ js/playListAPI.js
@@ -1,11 +1,13 @@
 const videoBox = document.querySelector('.video');
-const playListUl = document.querySelector('.playListUl');
+const kpopPlayListUl = document.querySelector('.kpopPlayListUl');
+const popPlayListUl = document.querySelector('.popPlayListUl')
 const key = "AIzaSyBJFWmVhHaFhVmTyg7PQPD9EJolH1AX4Vk";
 const kpopListId = "PLOHoVaTp8R7dfrJW5pumS0iD_dhlXKv17";
 const popListId = "PLOHoVaTp8R7d3L_pjuwIa6nRh4tH5nI4x";
 const ramdomIcon = document.querySelector('.fas');
 
 //video iframe
+//forIn 사용
 let player;
 let popPlayer;
 function onYouTubeIframeAPIReady(){
@@ -21,8 +23,8 @@ function onYouTubeIframeAPIReady(){
       'loop' : 0
     },
     events: {
-      'onReady' : kpopReady,
-      'onStateChange' : kpopStateChange,
+      'onReady' : ready,
+      'onStateChange' : stateChange,
     }
   })
 
@@ -38,7 +40,8 @@ function onYouTubeIframeAPIReady(){
       'loop' : 0
     },
     events: {
-      'onReady' : popReady,
+      'onReady' : ready,
+      'onStateChange' : stateChange
       
     }
   })
@@ -46,10 +49,12 @@ function onYouTubeIframeAPIReady(){
 
 //load 중에 함수 호출하면 오류뜸, 준비중일때 세팅
 //kpopReady
-function kpopReady(e){
+function ready(e){
+  console.log();
   //playlist
   const videoIdArr = e.target.getPlaylist();
   const promiseList = videoIdArr.map(getItem);
+  const playlistId = e.target.playerInfo.playlistId;
 
   function getItem(id){
     const videoApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${key}` 
@@ -65,40 +70,77 @@ function kpopReady(e){
 
   Promise.all(promiseList)
     .then(valuse => {
-      valuse.forEach((snippet, index) => {
-        const num = index+1;
-        const title = snippet.title;
-        const list = document.createElement('li');
-        const videoId = videoIdArr[index];
-
-        const output = `
-          <div class="num">${num}</div>
-          <div class="title">${title}</div>
-          <div class="playBtn">
-            <div class="play">&#9654</div>
-            <div class="stop">&#9632</div>
-          </div>
-          `
-        list.innerHTML = output;
-        list.classList.add(videoId);
-        playListUl.appendChild(list);
-      })
+      if(playlistId == kpopListId) {
+        //kpop
+        valuse.forEach((snippet, index) => {
+          const num = index+1;
+          const title = snippet.title;
+          const list = document.createElement('li');
+          const videoId = videoIdArr[index];
+  
+          const output = `
+            <div class="num">${num}</div>
+            <div class="title">${title}</div>
+            <div class="playBtn">
+              <div class="play">&#9654</div>
+              <div class="stop">&#9632</div>
+            </div>
+            `
+          list.innerHTML = output;
+          list.classList.add(videoId);
+          kpopPlayListUl.appendChild(list);
+        })
+      } else {
+        //pop
+        valuse.forEach((snippet, index) => {
+          const num = index+1;
+          const title = snippet.title;
+          const list = document.createElement('li');
+          const videoId = videoIdArr[index];
+  
+          const output = `
+            <div class="popNum">${num}</div>
+            <div class="popTitle">${title}</div>
+            <div class="popPlayBtn">
+              <div class="popPlay">&#9654</div>
+              <div class="popStop">&#9632</div>
+            </div>
+            `
+          list.innerHTML = output;
+          list.classList.add(videoId);
+          popPlayListUl.appendChild(list);
+        })
+      }
+      
     })
     .then(() => {
-      //stop
-      const stopBtn = document.querySelectorAll('.stop');
-      stopBtn.forEach((item) => item.addEventListener('click', stop))
-      //click play
-      const playBtn = document.querySelectorAll('.play');
-      playBtn.forEach((item, index) => item.addEventListener('click', () => play(index)))
-    })
+      //stop, click play
+      if(playlistId == kpopListId){
+        //kpop
+        const stopBtn = document.querySelectorAll('.stop');
+        stopBtn.forEach((item) => item.addEventListener('click', stop))
 
+        const playBtn = document.querySelectorAll('.play');
+        playBtn.forEach((item, index) => item.addEventListener('click', () => play(index)))
+      } else {
+        //pop
+        const stopBtn = document.querySelectorAll('.popStop');
+        stopBtn.forEach((item) => item.addEventListener('click', popStop))
+
+        const playBtn = document.querySelectorAll('.popPlay');
+        playBtn.forEach((item, index) => item.addEventListener('click', () => popPlay(index)))
+      }
+      
+    })
     const stop = () => player.pauseVideo();
     const play = (index) => player.playVideoAt(index);
+    const popStop = () => popPlayer.pauseVideo();
+    const popPlay = (index) => popPlayer.playVideoAt(index);
 }
 
 
-function kpopStateChange(e){
+function stateChange(e){
+  //playlist click color change
   if(e.data == 1){
     const playId = e.target.playerInfo.videoData.video_id;
     const list = document.querySelectorAll('.playListUl li');
@@ -123,3 +165,5 @@ function kpopStateChange(e){
 function popReady(e){
   console.log(e.target)
 }
+
+
